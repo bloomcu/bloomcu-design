9e7896e35264db4e23aaea39fdd10ba52a91ea78
Ryan Harmon <heyharmon@gmail.com>
2022-08-23 17:37:18 -0600

    Clean up tabs

--- src/js/store/useDesignStore.js
+++ src/js/store/useDesignStore.js
@@ -2,77 +2,82 @@ import { defineStore, acceptHMRUpdate } from 'pinia'
 import { designApi as DesignApi } from '../api/designApi'
 
 export const useDesignStore = defineStore('designStore', {
-    state: () => ({
-        designs: null,
-        design: null,
-        loading: true,
-    }),
-    
-    getters: {
-      variables: (state) => state.design.variables
+  state: () => ({
+    user: null,
+    designs: null,
+    design: null,
+    loading: true,
+  }),
+
+  getters: {
+    variables: (state) => state.design.variables
+  },
+
+  actions: {
+    init() {
+      this.loading = true
     },
-    
-    actions: {
-        index(params) {
-          this.loading = true
-          this.designs = null
+
+    index(params) {
+      this.loading = true
+      this.designs = null
+
+      DesignApi.index('bloomcu', params)
+        .then(response => {
+          this.designs = response.data.data
+          this.loading = false
+        }).catch(error => {
+          console.log('Error', error.response.data)
+        })
+    },
+
+    async store(design) {
+      this.loading = true
+
+      await DesignApi.store('bloomcu', design)
+        .then(response => {
+          this.designs.push(response.data.data)
+        }).catch(error => {
+          console.log('Error', error.response.data)
+        })
+    },
+
+    show(id) {
+      this.loading = true
+
+      DesignApi.show('bloomcu', id)
+        .then(response => {
+          this.design = response.data.data
           
-          DesignApi.index('bloomcu', params)
-            .then(response => {
-              this.designs = response.data.data
-              this.loading = false
-            }).catch(error => {
-              console.log('Error', error.response.data)
-            })
-        },
-        
-        async store(design) {
-          this.loading = true
-          
-          await DesignApi.store('bloomcu', design)
-            .then(response => {
-              this.designs.push(response.data.data)
-            }).catch(error => {
-              console.log('Error', error.response.data)
-            })
-        },
-        
-        show(id) {
-          this.loading = true
-          
-          DesignApi.show('bloomcu', id)
-            .then(response => {
-              this.design = response.data.data
-              
-              setTimeout(() => {
-                  this.loading = false
-              }, 1000)
-            })
-        },
-        
-        update() {
-          this.loading = true
-          
-          DesignApi.update('bloomcu', this.design.id, this.design)
-            .then(response => {
-              console.log('Design successfully updated')
-              
-              setTimeout(() => {
-                  this.loading = false
-              }, 2000)
-            })
-        },
-        
-        destroy(id) {
-          this.loading = true
-          
-          DesignApi.destroy('bloomcu', id)
-            .then(response => {
-              this.designs = this.designs.filter((design) => design.id !== id)
-              this.loading = false
-            })
-        },
+          setTimeout(() => {
+            this.loading = false
+          }, 1000)
+        })
+    },
+
+    update() {
+      this.loading = true
+
+      DesignApi.update('bloomcu', this.design.id, this.design)
+        .then(response => {
+          console.log('Design successfully updated')
+
+          setTimeout(() => {
+            this.loading = false
+          }, 2000)
+        })
+    },
+
+    destroy(id) {
+      this.loading = true
+
+      DesignApi.destroy('bloomcu', id)
+        .then(response => {
+          this.designs = this.designs.filter((design) => design.id !== id)
+          this.loading = false
+        })
     }
+  }
 })
 
 /**
@@ -80,5 +85,5 @@ export const useDesignStore = defineStore('designStore', {
  * https://pinia.vuejs.org/cookbook/hot-module-replacement.html
  */
 if (import.meta.hot) {
-    import.meta.hot.accept(acceptHMRUpdate(useDesignStore, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useDesignStore, import.meta.hot))
 }
