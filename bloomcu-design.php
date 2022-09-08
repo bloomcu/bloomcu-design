2d54140981be0fcc21aed4f1cf7457d7e971223a
Ryan Harmon <heyharmon@gmail.com>
2022-09-08 14:42:32 -0600

    Cleanup Assets class and add bust main.js

--- bloomcu-design.php
+++ bloomcu-design.php
@@ -131,7 +131,7 @@ final class Design_Plugin {
 		define( 'DESIGN_PLUGIN_PATH', dirname( DESIGN_PLUGIN_FILE ) );
 		define( 'DESIGN_PLUGIN_INCLUDES', DESIGN_PLUGIN_PATH . '/includes' );
 		define( 'DESIGN_PLUGIN_URL', plugins_url( '', DESIGN_PLUGIN_FILE ) );
-		define( 'DESIGN_PLUGIN_ASSETS', plugin_dir_url(__FILE__) . '/assets' );
+		define( 'DESIGN_PLUGIN_ASSETS', plugin_dir_url(__FILE__) . 'assets' );
 	}
 
 	/**
--- includes/class-assets.php
+++ includes/class-assets.php
@@ -32,7 +32,25 @@ class Assets {
 	 */
 	public function init() {
 		add_action('wp_enqueue_scripts', [$this, 'dequeue_child_theme_stylesheet'], 999);
-		add_action('wp_enqueue_scripts', [$this, 'register']);
+		add_action('wp_enqueue_scripts', [$this, 'enqueue_design_plugin_frontend']);
+	}
+	
+	/**
+	 * Disable child theme styles
+	 *
+	 * @return void
+	 */
+	public function dequeue_child_theme_stylesheet() {
+		wp_dequeue_style('client-css');
+	}
+	
+	/**
+	 * Enqueue design plugin frontend script
+	 *
+	 * @return void
+	 */
+	public function enqueue_design_plugin_frontend() {
+		wp_enqueue_script('design-plugin-frontend', DESIGN_PLUGIN_ASSETS . '/js/main.js?v=' . filemtime(DESIGN_PLUGIN_PATH . '/assets/js/main.js'), [], '', true);
 	}
 	
 	/**
@@ -63,164 +81,4 @@ class Assets {
 		
 		return urlencode($protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 	}
-	
-	/**
-	 * Disable child theme styles
-	 *
-	 * @return void
-	 */
-	public function dequeue_child_theme_stylesheet() {
-		// Dequeue child theme css
-		wp_dequeue_style('client-css');
-		
-		// Re enqueue base.js with depending on Vue
-		// wp_deregister_script('base');
-		// wp_enqueue_script('base', THEME_JS . '/app.js', [], '', true);
-		
-		// Dequeue search js and vue
-		// wp_dequeue_script('codybloom-search');
-		// wp_dequeue_script('vue');
-		
-		// Enqueue this plugin's js
-		wp_enqueue_script('design-plugin-frontend');
-	}
-	
-	// public function enqueue_dynamic_stylesheet() {
-	// 	wp_enqueue_style('dynamic-css', DESIGN_PLUGIN_ASSETS . '/css/style.php');
-	// }
-
-	/**
-	 * Register our app scripts and styles
-	 *
-	 * @return void
-	 */
-	public function register() {
-		$this->register_scripts( $this->get_scripts() );
-		// $this->register_styles( $this->get_styles() );
-	}
-
-	/**
-	 * Register scripts
-	 *
-	 * @param  array $scripts
-	 *
-	 * @return void
-	 */
-	private function register_scripts( $scripts ) {
-		foreach ( $scripts as $handle => $script ) {
-			$deps      = isset( $script['deps'] ) ? $script['deps'] : false;
-			$in_footer = isset( $script['in_footer'] ) ? $script['in_footer'] : false;
-			$version   = isset( $script['version'] ) ? $script['version'] : DESIGN_PLUGIN_VERSION;
-		
-			wp_register_script( $handle, $script['src'], $deps, $version, $in_footer );
-		}
-	}
-
-	/**
-	 * Localize admin script
-	 */
-	public function localize_admin_script() {
-		// wp_localize_script(
-		// 	'designPlugin-admin',
-		// 	'designLocal',
-		// 	[
-		// 		'siteUrl'  => esc_url( home_url() ),
-		// 		'ajaxUrl'  => admin_url( 'admin-ajax.php' ),
-		// 		'nonce'    => wp_create_nonce( 'wp_rest' ),
-		// 	]
-		// );
-	}
-
-	/**
-	 * Localize frontend script
-	 */
-	public function localize_frontend_script() {
-		// wp_localize_script(
-		// 	'design-plugin-frontend',
-		// 	'designLocal',
-		// 	[
-		// 		'siteUrl'  => esc_url( home_url() ),
-		// 	]
-		// );
-	}
-
-	/**
-	 * Register styles
-	 *
-	 * @param array $styles
-	 *
-	 * @return void
-	 */
-	public function register_styles( $styles ) {
-		// foreach ( $styles as $handle => $style ) {
-		// 	$deps = isset( $style['deps'] ) ? $style['deps'] : false;
-		// 
-		// 	wp_register_style( $handle, $style['src'], $deps, DESIGN_PLUGIN_VERSION );
-		// }
-	}
-
-	/**
-	 * Get all registered scripts
-	 *
-	 * @return array
-	 */
-	public function get_scripts() {
-		// $prefix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.min' : '';
-		// 
-		$scripts = [
-		// 	'designPlugin-vendor' => [
-		// 		'src'       => DESIGN_PLUGIN_ASSETS . '/js/vendor.min.js',
-		// 		'version'   => filemtime( DESIGN_PLUGIN_PATH . '/assets/js/vendor.min.js' ),
-		// 		'in_footer' => true,
-		// 	],
-			'design-plugin-frontend' => [
-				'src'       => DESIGN_PLUGIN_ASSETS . '/js/main.js',
-				// 'version'   => filemtime( DESIGN_PLUGIN_PATH . '/assets/js/main.js' ),
-				'in_footer' => true,
-			],
-			// 'designPlugin-admin' => [
-			// 	'src'       => DESIGN_PLUGIN_ASSETS . '/js/admin.min.js',
-			// 	'deps'      => [ 'jquery', 'designPlugin-vendor' ],
-			// 	'version'   => filemtime( DESIGN_PLUGIN_PATH . '/assets/js/admin.min.js' ),
-			// 	'in_footer' => true,
-			// ],
-			// 'font-awesome' => [
-			// 	'src'       => 'https://use.fontawesome.com/releases/v5.1.0/js/all.js',
-			// 	'deps'      => [],
-			// 	'version'   => '5.1.0',
-			// 	'in_footer' => true,
-			// ],
-			// 'polyfill-io' => [
-			// 	'src'       => 'https://cdn.polyfill.io/v2/polyfill.min.js',
-			// 	'deps'      => [],
-			// 	'version'   => '3.25.1',
-			// 	'in_footer' => false,
-			// ],
-		];
-
-		return $scripts;
-	}
-
-	/**
-	 * Get registered styles
-	 *
-	 * @return array
-	 */
-	public function get_styles() {
-
-		// $styles = [
-		// 	'designPlugin-style' => [
-		// 		'src' =>  DESIGN_PLUGIN_ASSETS . '/css/style.css'
-		// 	],
-		// 	'design-plugin-frontend' => [
-		// 		'src' =>  DESIGN_PLUGIN_ASSETS . '/css/frontend.css'
-		// 	],
-		// 	'designPlugin-admin' => [
-		// 		'src' =>  DESIGN_PLUGIN_ASSETS . '/css/admin.css'
-		// 	],
-		// ];
-		// 
-		// return $styles;
-	}
-
 }
